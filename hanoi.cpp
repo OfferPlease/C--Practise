80f61c3ea0fa3e43b112a2f47472fb21712d7b08
OfferPlease <zhouyuxin0106@gmail.com>
2024-02-26 16:45:07 -0800

    add hanoi

---
+++ hanoi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// (1) Only one disk can be moved at a time.
+// (2) A disk is slid off the top of one tower onto another tower.
+// (3) A disk cannot be placed on top of a smaller disk. Write a program to move the disks from the first tower to the
+/* show the process of moving all the disks from one tower to the other */
+int main()
+{
+    return 0;
+}
+
+void hanoi(int n, char src, char dst, char aux)
+{
+    if(n <= 0) return;
+}
